c2c3522d3130c9c931f6b6f79f66f549b8bc430a
Felipe-Bignoto <felipebignoto@gmail.com>
2024-07-19 17:45:52 -0300

    feat: update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,6 +8,9 @@ if (typeof window !== 'undefined') {
       apiKey: process.env.NEXT_PUBLIC_API_KEY,
       authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
       projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDERID,
+      appId: process.env.NEXT_PUBLIC_APP_ID,
     })
   }
 }
