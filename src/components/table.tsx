263095958e537f7a2a02cd59d29a0747e2edb711
Felipe-Bignoto <felipebignoto@gmail.com>
2024-07-23 15:53:10 -0300

    chore: centralized button

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -10,7 +10,7 @@ interface TabelaProps {
 
 export default function Table(props: TabelaProps) {
   return (
-    <table className="w-64 md:w-fit border-2 border-black overflow-hidden rounded-lg bg-white m-1">
+    <table className="w-64 md:w-fit border-2 border-black overflow-hidden rounded-lg bg-white m-1 ">
       <thead className="">
         <tr className="bg-gray-300">
           <th className="text-center p-1 md:p-4">Nome</th>
@@ -23,7 +23,7 @@ export default function Table(props: TabelaProps) {
         {props.item?.map((item, i) => (
           <tr
             key={item.itemId}
-            className={`${i % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}`}
+            className={`${i % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'} `}
             onClick={() => props.itemSelecionado?.(item)}
           >
             <td className="text-left py-1 md:py-2 md:px-4 whitespace-normal break-words">
@@ -36,23 +36,25 @@ export default function Table(props: TabelaProps) {
             <td className="h-min text-left py-1 md:py-2 md:px-4 whitespace-normal break-all">
               {item.itemObservacao}
             </td>
-            <td className="flex md:gap-2 py-1 m-auto md:py-2 md:px-4 justify-center items-center">
-              <Button
-                className="hover:bg-blue-600"
-                color="blue"
-                isIcon
-                onClick={() => props.itemSelecionado?.(item)}
-              >
-                <Pencil className="h-5 w-5 md:h-6 md:w-6" />
-              </Button>
-              <Button
-                className="hover:bg-red-600"
-                color="red"
-                isIcon
-                onClick={() => props.itemExcluido?.(item)}
-              >
-                <Trash2 className="h-5 w-5 md:h-6 md:w-6" />
-              </Button>
+            <td className="md:gap-2 py-1 md:py-2 md:px-4">
+              <div className="flex">
+                <Button
+                  className="hover:bg-blue-600"
+                  color="blue"
+                  isIcon
+                  onClick={() => props.itemSelecionado?.(item)}
+                >
+                  <Pencil className="h-5 w-5 md:h-6 md:w-6" />
+                </Button>
+                <Button
+                  className="hover:bg-red-600"
+                  color="red"
+                  isIcon
+                  onClick={() => props.itemExcluido?.(item)}
+                >
+                  <Trash2 className="h-5 w-5 md:h-6 md:w-6" />
+                </Button>
+              </div>
             </td>
           </tr>
         ))}
